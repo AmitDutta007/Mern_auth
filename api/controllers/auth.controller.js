c056b067f5bd7ceb33dfaf3d674c6862161bba18
amitdutta <duttaamit356@gmail.com>
2024-08-05 18:05:18 +0530

    Sign in functionality using redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import errorHandler  from '../utils/error.js'
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -30,7 +30,7 @@ export const signin = async (req, res, next) => {
         if (!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
-        const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: hashedPassword, ...rest } = validUser._doc;
         const expiryDate = new Date(Date.now() + 360000000000); // 1 hour
         res
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
-// import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
-    //   const { loading, error } = useSelector((state) => state.user);
+    // const [error, setError] = useState(false);
+    // const [loading, setLoading] = useState(false);
+      const { loading, error } = useSelector((state) => state.user);
 
     const navigate = useNavigate();
-    //   const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
@@ -23,8 +23,10 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            setLoading(true);
-            setError(false);
+            // setLoading(true);
+            // setError(false);
+
+            dispatch(signInStart())
             const res = await fetch('/api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -34,15 +36,19 @@ export default function SignIn() {
             });
             const data = await res.json();
             console.log(data);
-            setLoading(false);
-            navigate('/');
+            // setLoading(false);
+            
             if (data.success === false) {
-                setError(true);
+                // setError(true);
+                dispatch(signInFailure(data))
                 return;
             }
+            dispatch(signInSuccess(data))
+            navigate('/');
+            
         } catch (error) {
-            setLoading(false);
-            setError(true);
+            dispatch(signInFailure(error))
+
         }
     };
     return (
@@ -80,7 +86,7 @@ export default function SignIn() {
                 </Link>
             </div>
             <p className='text-red-700 mt-5'>
-                {error ? error.message || 'Something went wrong!' : ''}
+                {error ? error || 'Something went wrong!' : ''}
             </p>
         </div>
     );
